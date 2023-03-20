fbfc994a626adcfcd01a64e9ec3c076d8a7edcb7
Formantes <joformantes@lwsd.org>
2023-03-20 13:56:37 -0700

    made changes like functions to add divs

---
+++ Frogger/frogger.js
@@ -0,0 +1,62 @@
+let grid = document.querySelector('.grid')
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const startPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+
+let blocks = [];
+let logLeft = [];
+let logRight = [];
+let blockSetTwo = [];
+let carLeft = [];
+let carRight = [];
+let currentIndex = 76;
+
+function createDivs(storage, CSSclass) {
+    for (let i = 0; i < 9; i++) {
+        const block = document.createElement('div')
+        if (CSSclass != "") {
+            block.classList.add(CSSclass);
+        }
+        console.log("div added");
+        storage.push(block)
+        grid.appendChild(block);
+    }
+}
+
+function moveFrog(e) {
+    switch(e.key) {
+        case 'ArrowLeft':
+            console.log('move left');
+            break;
+        case 'ArrowRight':
+            console.log('move right');
+            break;
+        case 'ArrowUp':
+            console.log('move up');
+            break;
+        case 'ArrowDown':
+            console.log('move down');
+            break;
+
+    }
+    squares[currentIndex].classList.add('frog')
+
+}
+
+// createDivs(blocks, "");
+// createDivs(blocks, "");
+
+createDivs(logLeft, 'log-left');
+createDivs(logRight, 'log-right');
+createDivs(blockSetTwo, "");
+createDivs(carLeft, 'car-left');
+createDivs(carRight, 'car-right');
+
+// createDivs(blockSetThree, "");
+// createDivs(blockSetThree, "");
+
+// blocks[4] = blocks[4].classList.add('ending-block')
+// blockSetThree[13] = blockSetThree[13].classList.add('starting-block')
+
+document.addEventListener('keyup', moveFrog)
