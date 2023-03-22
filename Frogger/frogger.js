4f7e588368acbda74a86e540007136fe32927a26
matui3 <jonformantes@gmail.com>
2023-03-21 23:51:54 -0700

    almost done with frogger

--- Frogger/frogger.js
+++ Frogger/frogger.js
@@ -6,14 +6,20 @@ let gridLength = 9;
 let blocks = [];
 const width = 9;
 let currentIndex = 76
+let currentTime = 60;
+resultDisplay.textContent = currentTime
 
 createDivs();
-addLogClasses(blocks);
-addLClasses(blocks);
+addPreClasses(blocks, "log", 2, 4);
+addPreClasses(blocks, "car", 4, 6);
+addPostClasses(blocks, "l", 2, 4, 5);
+addPostClasses(blocks, "c", 4, 6, 3);
+
 
 const logsleft = document.querySelectorAll('.log-left');
 const logsRight = document.querySelectorAll('.log-right');
-
+const carsLeft = document.querySelectorAll('.car-left');
+const carsRight = document.querySelectorAll('.car-right')
 
 function createDivs() {
     for (let i = 0; i < gridLength * gridLength; i++) {
@@ -23,24 +29,26 @@ function createDivs() {
     }
 }
 
-function addLogClasses(blocks) {
-    for (let i = gridLength * 2; i < gridLength * 4; i++) {
-        str = "log"
-        str += i < gridLength * 3 ? "-left" : "-right"
-        blocks[i].classList.add(str)
+function addPreClasses(blocks, str, start, end) {
+    prefix = str;
+    for (let i = gridLength * start; i < gridLength * end; i++) {
+        prefix += i < gridLength * (start + end)/2 ? "-left" : "-right"
+        blocks[i].classList.add(prefix)
+        prefix = str;
     }
 }
 
-function addLClasses(blocks) {
+function addPostClasses(blocks, str, start, end, max) {
     j = 1;
-    for (let i = gridLength * 2; i < gridLength * 4; i++) {
-        if (j > 5) {
+    prefix = str;
+    for (let i = gridLength * start; i < gridLength * end; i++) {
+        if (j > max) {
             j = 1;
         }
-        str = "l"
-        str += j;
+        prefix += j;
         j++;
-        blocks[i].classList.add(str)
+        blocks[i].classList.add(prefix)
+        prefix = str;
     }
 }
 
@@ -68,7 +76,6 @@ function moveFrog(e) {
             }
             break;
     }
-    // console.log(squares[currentIndex])
     squares[currentIndex].classList.add('frog')
 }
 
@@ -80,30 +87,40 @@ const squares = document.querySelectorAll('.grid div')
 document.addEventListener('keyup', moveFrog)
 
 
-function autoMoveLogs() {
+function autoMoveElements() {
+    currentTime--;
+    timeLeftDisplay.textContent = currentTime;
     logsleft.forEach(logLeft => moveLogLeft(logLeft))
     logsRight.forEach(logRight => moveLogRight(logRight))
+    carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+    carsRight.forEach(carRight => moveCarRight(carRight))
+    lose();
+    win();
 }
 
-arr = ["l1", "l2", "l3", "l4", "l5"]
-
-
 function moveLogLeft(logLeft) {
-    let start = 0;
-    let end = start + 1;
-    if (end === arr.length) {
-       end = 0;
-    }
-    if (start === arr.length) {
-        start = 0;
-    }
     switch (true) {
-        case logLeft.classList.contains(arr[start]):
-            logLeft.classList.remove(arr[start]);
-            logLeft.classList.add(arr[end]);
+        case logLeft.classList.contains('l1'):
+            logLeft.classList.remove('l1');
+            logLeft.classList.add('l2');
+            break;
+        case logLeft.classList.contains('l2'):
+            logLeft.classList.remove('l2');
+            logLeft.classList.add('l3');
+            break;
+        case logLeft.classList.contains('l3'):
+            logLeft.classList.remove('l3');
+            logLeft.classList.add('l4');
+            break;
+        case logLeft.classList.contains('l4'):
+            logLeft.classList.remove('l4');
+            logLeft.classList.add('l5');
+            break;
+        case logLeft.classList.contains('l5'):
+            logLeft.classList.remove('l5');
+            logLeft.classList.add('l1');
             break;
     }
-    start++;
 }
 
 function moveLogRight(logRight) {
@@ -131,4 +148,58 @@ function moveLogRight(logRight) {
     }
 }
 
-setInterval(autoMoveLogs, 1000)
+function moveCarLeft(carLeft) {
+    switch (true) {
+        case carLeft.classList.contains('c1'):
+            carLeft.classList.remove('c1');
+            carLeft.classList.add('c2');
+            break;
+        case carLeft.classList.contains('c2'):
+            carLeft.classList.remove('c2');
+            carLeft.classList.add('c3');
+            break;
+        case carLeft.classList.contains('c3'):
+            carLeft.classList.remove('c3');
+            carLeft.classList.add('c1');
+            break;
+    }
+}
+
+function moveCarRight(carRight) {
+    switch (true) {
+        case carRight.classList.contains('c1'):
+            carRight.classList.remove('c1');
+            carRight.classList.add('c3');
+            break;
+        case carRight.classList.contains('c2'):
+            carRight.classList.remove('c2');
+            carRight.classList.add('c1');
+            break;
+        case carRight.classList.contains('c3'):
+            carRight.classList.remove('c3');
+            carRight.classList.add('c2');
+            break;
+    }
+}
+
+function lose() {
+    if (squares[currentIndex].classList.contains('c1') || 
+        squares[currentIndex].classList.contains('l4') || 
+        squares[currentIndex].classList.contains('l5')) {
+        resultDisplay.textContent = 'You lose!';
+        clearInterval(timerId)
+        squares[currentIndex].classList.remove('frog')
+        document.removeEventListener('keyup', moveFrog)
+    }
+}
+
+function win() {
+    if (squares[currentIndex].classList.contains('ending-block')) {
+        resultDisplay.textContent = 'You win!';
+        clearInterval(timerId);
+        document.removeEventListener('keyUp', moveFrog)
+    }
+}
+
+timerId = setInterval(autoMoveElements, 1000)
+
